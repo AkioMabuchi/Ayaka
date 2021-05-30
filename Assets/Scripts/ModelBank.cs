6c1eccedc687469b755e2548229cce28e336a80e
AkioMabuchi <mabuchiakio@gmail.com>
2021-05-30 17:20:59 +0900

    created ModelBank and GameManager

---
+++ Assets/Scripts/ModelBank.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ModelBank
+{
+    [RuntimeInitializeOnLoadMethod]
+    private static void GenerateModels()
+    {
+        Debug.Log("ModelBank");
+    }
+}
